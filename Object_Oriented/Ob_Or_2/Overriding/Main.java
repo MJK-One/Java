761d7acf73d1a43579e2736afc7d7216e7bca44d
unknown <hanleejss012@naver.com>
2025-12-10 19:10:28 +0900

    Java Overriding

---
+++ Object_Oriented/Ob_Or_2/Overriding/Main.java
@@ -0,0 +1,8 @@
+package Object_Oriented.Ob_Or_2.Overriding;
+
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.introduceFamily(); // 출력 "오버라이드"
+    }
+}
