d72d09656c4b0b6bc65ca5231ef0d1b30f16c2aa
unknown <hanleejss012@naver.com>
2025-12-15 21:28:00 +0900

    추상 계층

---
+++ Object_Oriented/Ob_Or_3/Abstraction_Layer/Animal.java
@@ -0,0 +1,5 @@
+package Object_Oriented.Ob_Or_3.Abstraction_Layer;
+
+public interface Animal extends LifeForm {
+    void makeSound(); //✅ 공통: 모든 동물은 소리를 냅니다.
+}
---
+++ Object_Oriented/Ob_Or_3/Abstraction_Layer/LifreForm.java
@@ -0,0 +1,5 @@
+package Object_Oriented.Ob_Or_3.Abstraction_Layer;
+
+public interface LifreForm {
+    void exist(); // ✅ 공통: 모든 생명체는 존재한다.
+}
