ab07f8af62f18bf05cc4a5eaa7b56aac46aa2fe3
unknown <hanleejss012@naver.com>
2025-12-16 21:49:51 +0900

    추상 계층

---
+++ Object_Oriented/Ob_Or_3/Abstraction_Layer/Cat.java
@@ -0,0 +1,17 @@
+package Object_Oriented.Ob_Or_3.Abstraction_Layer;
+
+public class Cat implements Animal {
+    @Override
+    public void exist() {
+        System.out.println("고양이가 존재합니다.");
+    }
+    
+    @Override
+    public void makeSound() {
+        System.out.println("야옹");
+    }
+
+    public void scratch() {
+        System.out.println("스크래치");
+    }
+}
--- Object_Oriented/Ob_Or_3/Abstraction_Layer/LifeForm.java
+++ Object_Oriented/Ob_Or_3/Abstraction_Layer/LifeForm.java
@@ -1,5 +1,5 @@
 package Object_Oriented.Ob_Or_3.Abstraction_Layer;
 
-public interface LifreForm {
+public interface LifeForm {
     void exist(); // ✅ 공통: 모든 생명체는 존재한다.
 }
