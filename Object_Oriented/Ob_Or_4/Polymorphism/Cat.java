487aa5fffd5c3e17e3c6eed4235c2f0399e04717
unknown <hanleejss012@naver.com>
2025-12-18 21:49:57 +0900

    다형성

---
+++ Object_Oriented/Ob_Or_4/Polymorphism/Cat.java
@@ -0,0 +1,17 @@
+package Object_Oriented.Ob_Or_4.Polymorphism;
+
+public class Cat implements Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("야옹");
+    }
+
+    @Override
+    public void exist() {
+        System.out.println("고양이가 존재합니다.");
+    }
+
+    public void scratch() {
+        System.out.println("스크래치!");
+    }
+}
