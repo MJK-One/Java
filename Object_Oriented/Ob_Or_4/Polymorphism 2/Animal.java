538acc64284ff9aafc6b5e737bbcdbfbdae0b90d
unknown <hanleejss012@naver.com>
2025-12-21 23:05:27 +0900

    자바 예제 2

---
+++ Object_Oriented/Ob_Or_4/Polymorphism 2/Animal.java
@@ -0,0 +1,5 @@
+package Object_Oriented.Ob_Or_4.Polymorphism;
+
+public interface Animal extends LifeForm {
+    void makeSound();
+}
---
+++ Object_Oriented/Ob_Or_4/Polymorphism 2/LifeForm.java
@@ -0,0 +1,5 @@
+package Object_Oriented.Ob_Or_4.Polymorphism;
+
+public interface LifeForm {
+    void exist();
+}
