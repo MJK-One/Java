1aa56b7d615eb96c90cc0ca93984c14f22dd3927
unknown <hanleejss012@naver.com>
2025-12-22 22:39:57 +0900

    Java 예제 삭제

--- Object_Oriented/Ob_Or_4/Polymorphism 2/Animal.java
+++
@@ -1,5 +0,0 @@
-package Object_Oriented.Ob_Or_4.Polymorphism;
-
-public interface Animal extends LifeForm {
-    void makeSound();
-}
--- Object_Oriented/Ob_Or_4/Polymorphism 2/LifeForm.java
+++
@@ -1,5 +0,0 @@
-package Object_Oriented.Ob_Or_4.Polymorphism;
-
-public interface LifeForm {
-    void exist();
-}
